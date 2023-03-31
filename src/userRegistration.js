f435def6b3bd650e94fef8de619dbe58c2dbaaba
naskavinda <naskavinda@gmail.com>
2023-04-01 00:13:31 +0530

    add userRegistration endpoint

--- src/userRegistration.js
+++ src/userRegistration.js
@@ -4,12 +4,12 @@ const DynamoDB = require('serverless-dynamodb-client');
 const dynamoDb = DynamoDB.doc;
 
 exports.handler = async (event) => {
+  console.log(event)
+  console.log(event.body)
+  let userDetails = JSON.parse(event.body)
   const params = {
     TableName: 'serverless-rest-api',
-    Item: {
-      id: '123',
-      name: 'John Doe'
-    }
+    Item: userDetails
   };
 
   await dynamoDb.put(params).promise();
